fba91de39394443c8701ff2a0da5535c1f7d3ff0
Artem Shkhmtv <artem.uralovich@yandex.ru>
2023-08-13 00:30:55 +0300

    отладка корс

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3001',
-    'https://art.nomoreparties.co',
+    'https://art.nomoreparties.co/',
   ],
   credentials: true,
 }));
